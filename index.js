f54ee388200ed650bfeab2e0540a83a80260c743
Ryan <ryan.kiprop@student.moringaschool.com>
2025-03-14 15:59:32 +0300

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    return function namedFunction() {
+      return "I am a named function";
+    };
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function() {
+      return "I am an anonymous function";
+    };
+  }
+  
+  
